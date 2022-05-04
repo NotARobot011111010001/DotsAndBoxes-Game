6cf779291c497abeb69f4bff2fb53549357788df
Muhammad Haziq Gul <hazigul12@gmail.com>
2022-05-04 19:38:22 +0100

    updated drawLine() - passes 794 tests

--- logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
+++ logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
@@ -2,6 +2,7 @@ package org.example.student.dotsboxgame
 
 import uk.ac.bournemouth.ap.dotsandboxeslib.*
 import uk.ac.bournemouth.ap.lib.matrix.*
+import java.lang.Exception
 import kotlin.random.Random
 
 
@@ -16,8 +17,8 @@ class StudentDotsBoxGame(val columns: Int, val rows: Int, players: List<Player>)
     override val boxes: Matrix<StudentBox> = MutableMatrix(columns, rows, ::StudentBox)
     //TODO("Create a matrix initialized with your own box type")
 
-    override val lines: SparseMatrix<StudentLine> = MutableSparseMatrix(columns+1, rows*2+1, { x, y -> x < columns || y % 2 == 1 }, ::StudentLine)
-    //MutableMatrix(columns+1, rows*2+1, ::StudentLine)
+    override val lines: SparseMatrix<StudentLine> = MutableSparseMatrix(columns+1, rows*2+1,
+        { x, y -> x < columns || y % 2 == 1 }, ::StudentLine)
     //TODO("Create a matrix initialized with your own line type")
 
     override var isFinished: Boolean = false
@@ -43,21 +44,19 @@ class StudentDotsBoxGame(val columns: Int, val rows: Int, players: List<Player>)
 
         override val adjacentBoxes: Pair<StudentBox?, StudentBox?>
             get() {
-                //return boxes[0,0] to boxes[0,0]
-
                 if (lineX == 0 && lineY % 2 ==1) {
                     val boxLeft = null
                     val boxRight = boxes[lineX, lineY / 2]
                     return Pair(boxLeft,boxRight)
                 }
-                else if (lineY == 0) {
-                    val boxLeft = null
-                    val boxRight = boxes[lineX, lineY]
-                    return Pair(boxLeft, boxRight)
-                }
                 else if (lineX == columns) {
                     val boxLeft = boxes[lineX - 1, lineY / 2]
                     val boxRight = null
+                    return Pair(boxLeft, boxRight)
+                }
+                else if (lineY == 0) {
+                    val boxLeft = null
+                    val boxRight = boxes[lineX, lineY]
                     return Pair(boxLeft, boxRight)
                 }
                 else if (lineY == rows * 2) {
@@ -76,7 +75,7 @@ class StudentDotsBoxGame(val columns: Int, val rows: Int, players: List<Player>)
                     return Pair(boxLeft, boxRight)
                 }
                 // TODO("You need to look up the correct boxes for this to work")
-            }
+            } // end of adjacentBoxes
 
         override fun drawLine() {
 
@@ -94,61 +93,65 @@ class StudentDotsBoxGame(val columns: Int, val rows: Int, players: List<Player>)
                         }
                     }
                 }
+            } else {
+                throw Exception("Cannot draw line")
             }
             fireGameChange()
+
+            if (!twoTurns) {
+                currentPlayer = if (currentPlayer == players[0]) { // rotates the player
+                    players[1]
+                } else {
+                    players[0]
+                }
+
+                if (currentPlayer is ComputerPlayer) { // checks if current player is computer
+                    // currentPlayer = players[1]
+                    playComputerTurns() // plays the turn
+                    this.isDrawn = true
+                    var finalLine = this
+                    for (boxes in adjacentBoxes.toList()) {
+                        if (boxes != null) {
+                            val computerBox = boxes.boundingLines.all { it.isDrawn }
+                            if (computerBox) {
+                                boxes.owningPlayer = currentPlayer
+                            }
+                        }
+                    }
+                }
+            } // end 0f (!twoTurns)
 
             var gameOverFlag = boxes.all { it.owningPlayer != null } // checks if all boxes owned
 
             if (gameOverFlag) {
-                isFinished =true
-                getScores()
-                //fireGameOver()
-            }
+                isFinished = true // sets the game to 'finished'
 
-            if (!twoTurns) {
-                currentPlayer = players[1]
-                playComputerTurns()
-                this.isDrawn = true
-                var finalLine = this
-                for (boxes in adjacentBoxes.toList()) {
-                    if (boxes != null) {
-                        val computerBox = boxes.boundingLines.all { it.isDrawn }
-                        if (computerBox) {
-                            boxes.owningPlayer = currentPlayer
-                        }
-                    }
-                }
-            }
-            fireGameChange()
-            if (this.isDrawn) {
-                //currentPlayer
-                for (boxes in adjacentBoxes.toList()) {
-                    if (boxes != null) {
-                        val LinesDrawn = boxes.boundingLines.all { it.isDrawn }
-                    }
-                }
-            }
+                var scores = getScores().mapIndexed { index, score -> players[index] to score}
+                fireGameOver(scores)
+            } // end of gameOverFlag
 
             //TODO("Implement the logic for a player drawing a line. Don't forget to inform the listeners (fireGameChange, fireGameOver)")
             // NOTE read the documentation in the interface, you must also update the current player.
-        }
-    }
+        } // end of drawLine()
+
+    } // end of StudentLine()
 
     inner class StudentBox(boxX: Int, boxY: Int) : AbstractBox(boxX, boxY) {
-        override var owningPlayer: Player? = null //players[0] ** this will change as the game progresses
+        override var owningPlayer: Player? = null // this will change as the game progresses
         //TODO("Provide this getter. Note you can make it a var to do so")
 
         /**
+         * Gets the bordering lines of each box
          * This must be lazy or a getter, otherwise there is a chicken/egg problem with the boxes
          */
         override val boundingLines: Iterable<DotsAndBoxesGame.Line>
-            get() = listOf(lines[boxX, (boxY*2)],
-                lines[boxX+1, (boxY*2) +1],
-                lines[boxX, (boxY+1)*2],
-                lines[boxX, (boxY*2) +1])
-
-
-    //lines //.filter { it.isDrawn }
+            get() =
+                listOf(
+                    lines[boxX, (boxY * 2)],
+                    lines[boxX + 1, (boxY * 2) + 1],
+                    lines[boxX, (boxY + 1) * 2],
+                    lines[boxX, (boxY * 2) + 1]
+                )
         //TODO("Look up the correct lines from the game outer class")
     }
 }
